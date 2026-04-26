4ccbbad6e7b7f15098c436a7fbd3b40859383472
DannaCojocari <danielacojocari08@gmail.com>
2026-04-27 00:15:23 +0300

    Set the project for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DestinationDetails from "./pages/DestinationDetails";
 import AddDestination from "./pages/AddDestination";
